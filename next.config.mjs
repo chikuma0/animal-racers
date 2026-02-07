de5bb338f726dae8dfdb7b17f05fdc3fbb22d952
Chikuma <chikuma@dera.ai>
2026-02-08 08:43:21 +0900

    Add static export config + base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/animal-racers',
+  images: { unoptimized: true },
+};
 
 export default nextConfig;
